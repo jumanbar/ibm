cf5f2435f1098c401693dcbcccf6337429946ab2
Juan Manuel Barreneche <jumanbar@gmail.com>
2011-11-09 11:30:20 -0200

    Para usar tmp.R como script de corridas...

--- tmp.R
+++ tmp.R
@@ -1,24 +1,11 @@
+source('ibm.R')
 
+ex <- seq(-2, 1, len=24)
+Ms <- 10 ^ ex
 
-  B_c <- m_c * bmr0 * M ^ (bmrExp - 1)
+run <- ibm(landsDist_ = 4, levelSeeds = 1,
+           mpd0 = 6, sizeMode = "random",
+           trs0 = 0.1, yield = 2000, tfinal=20000,
+           M = Ms[23])
 
-  BMR <- bmr0 * M ^ bmrExp
-
-  B_c * M / m_c = bmr0 * M ^ bmrExp
-
-  
-
-#->Asume que k promedio es 2 (i.e.: average energy intake = 2 * BMR)
-#->El tejido de reserva tiene un valor diferente, del punto de vista
-#  energético, al tejido de normal. Para simplificar los cálculos,
-#  el valor 'minBio' es la suma del tejido normal y de reserva de un
-#  recién nacido, pero todo en el equivalente a tejido normal.
-#  Esta medida sirve para calcular en número de descendientes de cada
-#  adulto en el momento de reproducción.
-
-
- - Sistema intermedio entre el últmo cambio: me equivoqué, West et al. 2001 plantean un cálculo proporcional al tamaño corporal para el costo por mantenimiento (B_c * m / m_c). Los demás atributos tamaño dependientes van a seguir los escalamientos planteados originalmente (excepto por mei, ya que se puede considerar que dicho atributo es isométrico).
-
- - Se arreglaron varias cosas de la parte de elección de parche, sobre todo lo que tiene que ver con los cálculos de costos y beneficios.
-
- - trs0 = 0.1
+save(run, file='../saves/corrida-M=7.4.RData')
