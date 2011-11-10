ee54241c55b576fe5ca6e91aba1748f27d11499e
Juan Manuel Barreneche <jumanbar@gmail.com>
2011-11-10 21:40:26 -0200

    Agrega getNormSd a aux.ibm.R

--- aux.ibm.R
+++ aux.ibm.R
@@ -96,6 +96,24 @@ fixedObjects <- function() {
 }
 ####################################
 ####################################
+getNormSd <- function(x, init=1) {
+# La idea es que x normalmente sea la población.
+# Calcularía el desvío estándar normalizado del tamaño poblacional para
+# cada iteración. Esto se calcula:
+# 2 * ds / vp
+# ds: desvío standard de toda la serie (empezando desde init)
+# vp: varianza promedio de toda la serie (empezando desde init)
+  x <- x[init:length(x)]
+  v <- numeric(length(x) - 1)
+  for (i in 2:length(x))
+    v[i - 1] <- var(x[1:i])
+  s <- sqrt(v)
+  m <- cumsum(v) / (1:length(v))
+  o <- 2 * s / m
+  return(o)
+}
+####################################
+####################################
 getStats <- function(x, show=TRUE) {
 # x:
 #  1. Objeto ibm
